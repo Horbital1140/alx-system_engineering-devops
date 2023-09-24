7a557d48a755f5a07e36924bca7e2cc70aa7acc8
horbital1140 <adeemma1993@gmail.com>
2023-09-24 10:25:47 -0700

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#puppet class for maanging ssh config
+
+include ssh_config
+class ssh_config {
+file { '/etc/ssh/ssh_config':
+ensure 	=> present,
+content => "Host *
+                  identityFile ~/.ssh/school
+	 	   PasswordAuthentication no
+}
+}
