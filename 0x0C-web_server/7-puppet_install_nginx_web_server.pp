05d7cf88446d4d6a85310f49a72505cfc058bacf
horbital1140 <adeemma1993@gmail.com>
2023-09-26 09:33:06 -0700

    webserver

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,60 +1,6 @@
-# Install Nginx package
-package { 'nginx':
-  ensure => 'installed',
+# Installs a Nginx server
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
-
-# Ensure Nginx service is enabled and running
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
-  require => Package['nginx'],
-}
-
-# Configure Nginx server block
-file { '/etc/nginx/sites-available/default':
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html;
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-    }
-}
-  ",
-  notify => Service['nginx'],
-  require => Package['nginx'],
-}
-
-# Create the HTML files
-file { '/var/www/html/index.html':
-  content => "Hello World!\n",
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
-  require => Package['nginx'],
-}
-
-# Ensure Nginx listens on port 80
-file { '/etc/nginx/sites-available/default':
-  ensure => 'present',
-  require => Package['nginx'],
-  notify => Service['nginx'],
-}
-
-# Remove the default Nginx default site
-file { '/etc/nginx/sites-enabled/default':
-  ensure => 'absent',
-  require => File['/etc/nginx/sites-available/default'],
-  notify => Service['nginx'],
-}
-
